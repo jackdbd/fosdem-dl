95e4c688165b594a71f31777b26657390d92193f
Giacomo Debidda <jackdebidda@gmail.com>
2021-02-12 00:29:10 +0100

    Cleanup

--- README.md
+++ README.md
@@ -1,21 +1,32 @@
 # fosdem-dl (FOSDEM talks downloder)
 
-todo
+[babashka](https://github.com/babashka/babashka) script to download talks and attachments from the 2003-2020 [FOSDEM](https://fosdem.org/2021/) websites.
 
 ## Usage
 
-todo
+Here are some examples that show how to use the script.
+
+Show help.
 
 ```sh
-# download the talk in .webm format and all its attachments (PDF, slides, etc)
-./fosdem-dl.sh -a -f webm
-# help
 ./fosdem-dl.sh -h
+```
+
+Download all the 2018 talks from the python track in .webm format (default) without any attachments.
+
+```sh
+./fosdem-dl.sh -y 2018 -t python
+```
+
+Download all the 2020 talks from the web performance track in .mp4 format and include all attachments (PDF, slides, etc).
+
+```sh
+./fosdem-dl.sh -y 2020 -t web_performance -f mp4 -a
 ```
 
 ## Build
 
-todo
+In order to use this script you need to generate a GraalVM native image of the [pod-jaydeesimon-jsoup](https://github.com/jaydeesimon/pod-jaydeesimon-jsoup) babashka pod and place it in this project's root. This is necessary because pod-jaydeesimon-jsoup is not yet published on the [Pod registry](https://github.com/babashka/pod-registry).
 
 ## Tests
 
@@ -24,3 +35,14 @@ Run all tests with:
 ```sh
 ./test-runner.clj
 ```
+
+## TODO
+
+- Add GitHub action to run tests on push.
+- Improve error handling.
+- Add more tests.
+- Save edn file with all conference tracks over the years (to validate the `--track` option for a given `--year`).
+- Accept destination directory where to download the files.
+- Include the talk's links too? Maybe write them in a text/markdown file?
+- Show curl's progress bar. See [here](https://github.com/babashka/babashka.curl/issues/34).
+- Make a GUI with [pod-babashka-lanterna](https://github.com/babashka/pod-babashka-lanterna)? Probably not...
--- src/fosdem_dl/core.clj
+++ src/fosdem_dl/core.clj
@@ -4,38 +4,21 @@
   "CLI script to download the talks given at FOSDEM over the years
   https://fosdem.org/"
   (:require [clojure.string :as str]
-            [cheshire.core :as json]
             [clojure.tools.cli :refer [parse-opts]]
             [babashka.curl :as curl]
-            [babashka.pods :as pods]
-            [clojure.java.io :as io])
+            [babashka.pods :as pods])
   (:import [java.lang Thread]))
-
-
-(pods/load-pod 'org.babashka/etaoin "0.0.1")
 
 ;; pod-jaydeesimon-jsoup is not yet published on the babashka pod registry, so
 ;; we need to compile it as a GraalVM native-image
 ;; https://github.com/jaydeesimon/pod-jaydeesimon-jsoup
 (pods/load-pod "./pod-jaydeesimon-jsoup")
-
-(require '[pod.babashka.etaoin :as eta])
 (require '[pod.jaydeesimon.jsoup :as jsoup])
 
 ;; from 2013 to 2021 the structure of the webpage of any conference track stays the same
 
 (def schedule-page "https://fosdem.org/2021/schedule/")
 (def video-page "https://video.fosdem.org/")
-;; TODO: tracks as command line argument, or maybe from EDN file?
-(def track-page-example "https://archive.fosdem.org/2020/schedule/track/web_performance/")
-;; XPath selector for links in track webpages
-;; $x ("//table[last()]/tbody/tr/td[position() = 2]/a/@href")
-
-;; (def webm-example "https://video.fosdem.org/2020/H.1309/webperf_qoe_research.webm")
-(def webm-example "https://ftp.fau.de/fosdem/2020/H.1309/webperf_qoe_research.webm")
-(def mp4-example "https://ftp.fau.de/fosdem/2020/AW1.125/mgmtconfigmore.mp4")
-(def zip-example "http://ipv4.download.thinkbroadband.com/5MB.zip")
-;; (def zip-example "http://ipv4.download.thinkbroadband.com/200MB.zip")
 
 ;; There is a FOSDEM 2004 website, but I couldn't find any talk.
 (defn validate-year
@@ -46,7 +29,6 @@
   [format]
   (or (= "webm" format) (= "mp4" format)))
 
-;; TODO: video format: webm mp4
 (def cli-options
   [["-y" "--year YEAR" "Year of the FOSDEM conference"
     :default 2020
@@ -64,10 +46,9 @@
   (-> (Runtime/getRuntime)
       (.addShutdownHook (Thread. #(println "...")))))
 
-;; TODO: include the talk's links too? Maybe write them in a text/markdown file?
 (def usage-help
   (->> ["Usage: fosdem-dl [OPTION]..."
-        "Download talks given at the FOSDEM conference over the years."
+        "Download the talks given at FOSDEM over the years."
         ""
         "  -y, --year YEAR      Select given year (default: 2020)"
         "  -t, --track TRACK    Select a conference track (e.g. web_performance)"
@@ -77,65 +58,88 @@
         ""]
        (str/join \newline)))
 
-(defn screenshot
-  [year track]
-  (let [driver (eta/chrome)
-        url (str "https://archive.fosdem.org/" year "/schedule/track/" track "/")
-        fname (str year "-" track ".png")]
-    (eta/go driver url)
-    (eta/screenshot driver fname)
-    (eta/quit driver)))
+;; (defn- get-video
+;;   [url]
+;;   (let [resp (curl/get url {:debug true
+;;                             ;; :as :stream
+;;                             :raw-args ["-O" "--verbose"]
+;;                             :throw false})]
+;;     (when (= 200 (:status resp))
+;;       (println (str "video at " url " downloaded")))
+;;     (comment
+;;       (json/parse-string (:body resp))
+;;       (println "ERR " (:err resp))
+;;       (println "EXIT " (:exit resp))
+;;       (println "PROCESS " (:process resp))
+;;       (println "COMMAND " (:command resp))
+;;       (println "OPTIONS " (:options resp))
+;;       (println "HEADERS " (:headers resp)))))
 
-(defn- get-video
-  [url]
-  (let [resp (curl/get url {:debug true
-                            ;; :as :stream
-                            :raw-args ["-O" "--verbose"]
-                            :throw false})]
-    (when (= 200 (:status resp))
-      (println (str "video at " url " downloaded")))
-    (comment
-      (json/parse-string (:body resp))
-      (println "ERR " (:err resp))
-      (println "EXIT " (:exit resp))
-      (println "PROCESS " (:process resp))
-      (println "COMMAND " (:command resp))
-      (println "OPTIONS " (:options resp))
-      (println "HEADERS " (:headers resp)))))
-
-(defn m->href
+(defn- m->href
   [{attrs :attrs}]
   (let [end (get attrs "href")]
     (str "https://fosdem.org" end)))
 
-(defn url->hrefs
+(defn- url->hrefs!
+  "Fetch `url` and extract the href property from all <a> tags."
   [url]
-  (let [links (-> (curl/get url)
-                  :body
-                  (jsoup/select "table:last-child tr>td:nth-child(2)>a[href]"))
-        hrefs (map m->href links)]
-    hrefs))
+  (let [resp (curl/get url {:throw false})
+        status (:status resp)]
+    (if (= 200 status)
+      (let [links (-> resp
+                      :body
+                      (jsoup/select "table:last-child tr>td:nth-child(2)>a[href]"))
+            hrefs (map m->href links)]
+        hrefs)
+      (println (str "Cannot fetch " url " (status: " status ")")))))
 
-(defn- download-attachment
-  [url]
-  (let [resp (curl/get url {:debug true
-                            :raw-args ["-O"]
+(defn- download-video!
+  "Extract the <video> `src` attr from `body` and download the video file (in
+  the specified `format`) to location `dest`."
+  [body dest format]
+  (let [video-src (-> body
+                      (jsoup/select "video>source")
+                      first
+                      :attrs
+                      (get "src"))
+        [webm-filename room year] (->> (str/split video-src #"/")
+                                       reverse
+                                       (take 3))
+        webm-url (str "https://ftp.fau.de/fosdem/" year "/" room "/" webm-filename)]
+    (case format
+      "webm" (let [filepath (str dest webm-filename)
+                   _resp (curl/get webm-url {:raw-args ["-o" filepath]
+                                             :throw false})])
+      "mp4" (let [mp4-filename (-> (str/split webm-filename #"webm") first (str "mp4"))
+                  filepath (str dest mp4-filename)
+                  mp4-url (-> (str/split webm-url #"webm") first (str "mp4"))
+                  _resp (curl/get mp4-url {:raw-args ["-o" filepath]
+                                           :throw false})])
+      :else (println "format" format "not supported."))))
+
+(defn- download-attachment!
+  "Download the file found at `url` to location `dest`."
+  [url dest]
+  (let [
+        filename (-> (str/split url #"/") last)
+        filepath (str dest filename)
+        resp (curl/get url {:raw-args ["-o" filepath]
                             :throw false})]
-    (when (= 200 (:status resp))
-      (println url "downloaded"))))
+    (when-not (= 200 (:status resp))
+      (println url "Could not download" url))))
 
-(defn visit-page!
-  "Visit `url` and apply side effect `f!`"
-  [f! url]
-  (let [links (-> (curl/get url {:throw false})
-              :body
-              (jsoup/select ".event-attachments>li>a[href]"))
-        hrefs (map m->href links)]
-    ;; (println "Downloading attachments from" url)
-    (doseq [url hrefs]
-      (f! url))))
+;; (def download-attachments! (partial on-attachments download-attachment!))
 
-(def download-attachment-on-visit! (partial visit-page! download-attachment))
+(defn- download-all!
+  "Download video and attachments found on the `url` page."
+  [url dest format should-download-attachments]
+  (let [body (-> (curl/get url {:throw false}) :body)]
+    (when should-download-attachments
+      (let [links (jsoup/select body ".event-attachments>li>a[href]")
+            hrefs (map m->href links)]
+        (doseq [url hrefs]
+          (download-attachment! url dest))))
+    (download-video! body dest format)))
 
 (defn -main
   "You can run this program with [babashka](https://github.com/babashka/babashka):
@@ -147,26 +151,28 @@
     (System/exit 1))
   (shutdown-hook)
   (let [options (:options (parse-opts *command-line-args* cli-options))
-        coll-contains? (partial contains? options)
-        download-attachments (:attachments options)
-        hrefs (url->hrefs track-page-example)]
-    (when download-attachments
-      (doseq [url hrefs]
-        (download-attachment-on-visit! url)))
-    (when (coll-contains? :track)
-      (let [track (:track options)
-            year (:year options)]
-        (screenshot year track)))
-    (if (:help options)
+        coll-contains? (partial contains? options)]
+    (when (coll-contains? :help)
       (println usage-help)
-      (if (= "webm" (:format options))
-        (get-video webm-example)
-        (get-video mp4-example)))))
+      (System/exit 0))
+    (let [should-download-attachments (coll-contains? :attachments)
+          year (:year options)
+          track (:track options)
+          url (str "https://archive.fosdem.org/" year "/schedule/track/" track "/")
+        ;; TODO: make sure that this directory exists, otherwise curl fails (silently because we use :throw false)
+          dest (str "./downloads/")
+          hrefs (url->hrefs! url)]
+      (doseq [url hrefs]
+        (download-all! url dest (:format options) should-download-attachments)))))
 
 ;; check if the current file was the file invoked from the command line
 ;; https://book.babashka.org/#main_file
 (when (= *file* (System/getProperty "babashka.file"))
   (-main))
 ;; (-main *command-line-args*)
-;;
-;; TODO: how to show curl's progress bar?
+
+(comment
+  (-main {:year 2020
+          :track "web_performance"
+          :attachments true
+          :format "webm"}))
